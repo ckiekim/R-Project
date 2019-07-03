1cb14d0aad52defefb67619d617a755b890621fe
Cheolkie Kim <ckiekim@naver.com>
2019-07-03 19:33:34 +0900

    Naver Mail Crawling

--- 01_Crawling/naver_mail.R
+++ 01_Crawling/naver_mail.R
@@ -1,6 +1,9 @@
 # Naver 에 로그인하기
 library(RSelenium)
 library(rvest)
+library(stringr)
+trim <- function(x) gsub("^\\s+|\\s+$", "", x)
+
 remDr<-remoteDriver(remoteServerAddr="localhost", port=4445L, browserName="chrome")
 remDr$open()
 
@@ -9,16 +12,21 @@ txt_id <- remDr$findElement(using="id", value="id")
 txt_pw <- remDr$findElement(using="id", value="pw")
 login_btn <- remDr$findElement(using="class", value="btn_global")
 
-txt_id$setElementAttribute("value", "*****") # *에 아이디 입력
+txt_id$setElementAttribute("value", "ckiekim") # 아이디 입력
 txt_pw$setElementAttribute("value", "*****") # *에 비밀번호 입력
 login_btn$clickElement()
 
 remDr$navigate("https://mail.naver.com/")
-mail_subject <- remDr$findElement(using="class", value="subject")
+mail_texts <- remDr$findElement(using="id", value="list_for_view")
                             # (using = 'css selector', "subject")
-mail_subject$getElementText()
+mail_texts
+mail_texts <- mail_texts$getElementText()
+tmp <- str_split(mail_texts, '\n') %>% [[1]]
 
-#subjects<-unlist(lapply(mail_title, function(x){x$getElementText()}))
+for (i in 1:20) {
+  
+}
+#subjects<-unlist(lapply(mail_subjects, function(x){x$getElementText()}))
 #subjects
 
 remDr$close()
