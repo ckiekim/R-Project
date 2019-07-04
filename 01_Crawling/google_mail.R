68bb3708b51879176bca5088eaecedd925aa5f4a
Cheolkie Kim <ckiekim@naver.com>
2019-07-04 23:28:04 +0900

    Google Mail

--- 01_Crawling/google_mail.R
+++ 01_Crawling/google_mail.R
@@ -18,23 +18,27 @@ next_btn <- remDr$findElement(using="css selector", '.RveJvd.snByac')
 txt_pw$sendKeysToElement(list("*"))     # *에 비밀번호 입력
 next_btn$clickElement()
 
-mail_texts <- remDr$findElement(using="id", value="list_for_view")
-mail_texts
-mail_texts <- mail_texts$getElementText()
-tmp <- str_split(mail_texts, '\n') %>% .[[1]]
+html <- remDr$getPageSource()[[1]] 
+html <- read_html(html)
+
+html %>%
+  html_node('table.F.cf.zt') %>%
+  html_nodes('tbody>tr') -> trs
 
 sender <- c()
 subject <- c()
 time <- c()
-for (i in 1:20) {
-  sender <- c(sender, tmp[4*i-3])
-  subject <- c(subject, tmp[4*i-2])
-  time <- c(time, tmp[4*i-1])
+for (tr in trs) {
+  tr %>% 
+    html_node('td.yX.xY') %>%
+    html_node('div') %>%
+    html_nodes('span') -> spans
+  len <- length(spans)
+  sender <- c(sender, html_text(spans[1]))       # Sender
+  subject <- c(subject, html_text(spans[len-1]))     # Subject
+  time <- c(time, html_text(spans[len]))           # Time
+  
 }
 df_mail <- data.frame(sender=sender, subject=subject, time=time)
 df_mail
 remDr$close()
-
-#subjects<-unlist(lapply(mail_subjects, function(x){x$getElementText()}))
-#subjects
-
