12e3e44b50c2b65acad601af6f630eedf4b9043d
Cheolkie Kim <ckiekim@naver.com>
2019-07-04 22:22:04 +0900

    Google Mail

--- 01_Crawling/google_mail.R
+++ 01_Crawling/google_mail.R
@@ -10,19 +10,14 @@ remDr$open()
 remDr$navigate("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
 txt_id <- remDr$findElement(using="css selector", '#identifierId')
 next_btn <- remDr$findElement(using="css selector", '.RveJvd.snByac')
-txt_id$setElementAttribute("value", "ckiekim@gmail.com") # 아이디 입력
+txt_id$setElementAttribute("value", "ckiekim@gmail.com")     # 아이디 입력
 next_btn$clickElement()
 
-#remDr$navigate("https://accounts.google.com/signin/v2/sl/pwd?continue=https%3A%2F%2Fmail.google.com%2Fmail&passive=1209600&sacu=1&ignoreShadow=0&hl=ko&acui=0&flowName=GlifWebSignIn&flowEntry=AddSession&cid=1&navigationDirection=forward")
-#remDr$refresh()
 txt_pw <- remDr$findElement(using="css selector", '.whsOnd.zHQkBf')
-#txt_pw <- remDr$findElement(using="css selector", '.AxOyFc.snByac')
 next_btn <- remDr$findElement(using="css selector", '.RveJvd.snByac')
-txt_pw$setElementAttribute("value", "kim11067") # *에 비밀번호 입력
+txt_pw$sendKeysToElement(list("*"))     # *에 비밀번호 입력
 next_btn$clickElement()
-remDr$close()
-##########################################################
-remDr$navigate("https://mail.naver.com/")
+
 mail_texts <- remDr$findElement(using="id", value="list_for_view")
 mail_texts
 mail_texts <- mail_texts$getElementText()
