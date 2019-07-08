abf3f1888f8a9070459c121726595dbf3aeb515d
Cheolkie Kim <ckiekim@naver.com>
2019-07-08 21:37:24 +0900

    Cine Analysis

--- 01_Crawling/aladdin_review_analysis.R
+++ 01_Crawling/aladdin_review_analysis.R
@@ -21,15 +21,42 @@ trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 reply <- aladdin$review
 write.csv(reply, "reply1.csv", row.names = F)
-lines <- read.csv("reply1.csv")
+
+lines <- read.csv("D:/Workspace/R_Project/01_Crawling/reply1.csv")
+lines <- as.character(lines$x)
 
 raw_words <- sapply(lines, extractNoun, USE.NAMES = F)
 
 words <- unlist(raw_words)
 words <- str_replace_all(words, "[^[:alpha:]]", "")
 words <- Filter(function(x) {nchar(x) >= 2}, words)
-words <- Filter(function(x) {nchar(x) <= 20}, words)
+words <- Filter(function(x) {nchar(x) <= 10}, words)
 words <- gsub(" ", "", words)
 words <- gsub("\\d+", "", words)
-head(words, 20)
-str(words)
+
+write(unlist(words), "reply1.txt")
+reviews <- read.table("reply1.txt")
+wordcount <- table(reviews)
+head(sort(wordcount, decreasing = T), 30)
+
+rmwords <- c('영화', '진짜', 'ㅠㅠ', '관', '람객', '람')
+for (rmword in rmwords) {
+  words <- gsub(rmword, "", words)
+}
+words <- gsub('재밌게', '재미', words)
+words <- gsub('재밌고', '재미', words)
+words <- gsub('재밌어요', '재미', words)
+words <- gsub('재밌었어요', '재미', words)
+write(unlist(words), "reply1.txt")
+reviews <- read.table("reply1.txt")
+wordcount <- table(reviews)
+head(sort(wordcount, decreasing = T), 30)
+
+windowsFonts(font=windowsFont("맑은 고딕"))
+palete <- brewer.pal(12, 'Paired')
+par(mai=rep(0,4))
+wordcloud(names(wordcount), freq=wordcount, 
+          scale = c(7,0.3), rot.per = 0.1,
+          min.freq = 20, random.order = F, 
+          random.color = T, colors = palete, family="font")
+
